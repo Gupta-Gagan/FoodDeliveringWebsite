cbb2f2c1643f79773f7ac5e95d96c0c091ff48b7
Gagan-Gupta <gagan88775500@gmail.com>
2023-11-19 12:04:35 +0530

    SpeedyBites

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+// It is taken from Swiperjs.
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+
+
+//It is taken from swiperjs
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop:true,
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+window.onload = loading;
+
+function loading(){
+  setInterval(loader, 3000);
+}
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+
